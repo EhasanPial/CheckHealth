03602aa5bb1f07bad97d038c477ad14da890e19f
pial <pialkhanpial@gmail.com>
2022-07-06 11:08:06 +0600

    DataRepository Solved : Pial

--- app/src/main/java/com/pialroni/checkhealth/DataRepository.java
+++ app/src/main/java/com/pialroni/checkhealth/DataRepository.java
@@ -1,19 +1,9 @@
 package com.pialroni.checkhealth;
 
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.pialroni.checkhealth.IDataBase;
-import com.pialroni.checkhealth.UserData;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public abstract class DataRepository implements IDataBase {
